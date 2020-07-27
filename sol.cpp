f70fe82d9a87ca0e98697761bd9093451c53600a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-07-27 20:12:40 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void test_case() {
+	
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//int T;
+	//cin >> T;
+	//for(int nr = 1; nr <= T; ++nr) {
+		//test_case();
+	//}
+	test_case();
+	return 0;
+}
